2f519225f15a040357f3229978d5270b4e9f88a0
Sebastian Detert <github@elygor.de>
2013-05-25 11:23:42 +0200

    added small db class, not tested yet

---
+++ db/config.php
@@ -0,0 +1,16 @@
+<?php
+namespace de\detert\sebastian\slimline\Db;
+
+/**
+ * database configurations
+ *
+ * @author sebastian.detert <github@elygor.de>
+ * @date 25.05.13
+ * @time 10:18
+ * @license property of Sebastian Detert
+ */
+class Config {
+    public $dsn;
+    public $user;
+    public $password;
+}
